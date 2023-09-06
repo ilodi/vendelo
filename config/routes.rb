bf024d384ebefb3425386b4e2119c223248f65f9
ilodi <lodi.dev@outlook.com>
2023-09-06 09:28:47 -0600

    add categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-  #delete '/products/:id', to: 'products#destroy'
-  #update product
-  #patch '/products/:id', to: 'products#update'
-  #post '/products', to: 'products#create'
-  #el path(helper o as:) es para ayudar en las url a ser mas humanas
-  #get '/products/new', to: 'products#new', as: :new_product
-  #get '/products', to: 'products#index'
-  #as crea un helper para identificar mejor la url
-  #get '/products/:id', to: 'products#show', as: :product
-  #edit
-  #get '/products/:id/edit', to: 'products#edit', as: :edit_product
-  #todas las acciones de arriba estan en resources :products no se necesitan
+  resources :categories, except: :show
   resources :products, path: '/'
 end

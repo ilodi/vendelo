071d6a3736fba227cdd7aed52c3faafa6b4492fa
ilodi <lodi.dev@outlook.com>
2023-09-06 09:56:19 -0600

    done categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,27 +1,25 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[ show edit update destroy ]
 
-  # GET /categories or /categories.json
   def index
     @categories = Category.all
   end
 
-  # GET /categories/new
+
   def new
     @category = Category.new
   end
 
-  # GET /categories/1/edit
+
   def edit
   end
-
-  # POST /categories or /categories.json
+  
   def create
     @category = Category.new(category_params)
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to categories_url(@category), notice: "Category was successfully created." }
+        format.html { redirect_to categories_url(@category), notice: t('.created') }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -32,7 +30,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to categories_url(@category), notice: "Category was successfully updated." }
+        format.html { redirect_to categories_url(@category), notice: t('.updated') }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
